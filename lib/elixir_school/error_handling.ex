44f264f160738ae91fd296d93da8b184acfa8b67
ryuzaki <ryuzaki>
2021-08-13 21:47:08 +0530

    custom error

--- lib/elixir_school/error_handling.ex
+++ lib/elixir_school/error_handling.ex
@@ -2,12 +2,18 @@ defmodule ElixirSchool.ErrorHandling do
   def main do
     try do
       # raise "hacked!"
-      raise ArgumentError, message: "invalid"
+      # raise ArgumentError, message: "invalid"
+      raise CustomError
     rescue
       e in ArgumentError -> IO.puts("args error #{e.message}")
+      e in CustomError -> IO.puts("custom error #{e.message}")
       e -> IO.inspect(e.message)
     after
       IO.inspect("done rescuing")
     end
   end
+end
+
+defmodule CustomError do
+  defexception message: "custom error"
 end
