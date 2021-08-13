ba1225d91593c1c767c3b3efd8b0ee4983136f03
ryuzaki <ryuzaki>
2021-08-13 22:51:42 +0530

    exit

--- lib/elixir_school/error_handling.ex
+++ lib/elixir_school/error_handling.ex
@@ -11,7 +11,28 @@ defmodule ElixirSchool.ErrorHandling do
     after
       IO.inspect("done rescuing")
     end
+
+    # The throw/1 function gives us the ability to exit execution with a specific value we can catch and use
+    try do
+      for x <- 1..10 do
+        if x == 5 do
+          throw(x)
+        else
+          IO.puts("#{x}")
+        end
+      end
+    catch
+      x -> "Caught: #{x}"
+    end
   end
+
+  try do
+    exit("reason for exit")
+  catch
+    :exit, reason -> IO.inspect("exit - reason #{reason}")
+  end
+
+  # While it is possible to catch an exit with try/catch doing so is extremely rare. In almost all cases it is advantageous to let the supervisor handle the process exit:
 end
 
 defmodule CustomError do
