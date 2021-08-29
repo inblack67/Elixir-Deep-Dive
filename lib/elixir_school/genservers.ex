142b49ea6c54fd39886207be366e001c6c73c501
ryuzaki <ryuzaki>
2021-08-29 19:53:43 +0530

    genserver queue

---
+++ lib/elixir_school/genservers.ex
@@ -0,0 +1,30 @@
+defmodule ElixirSchool.Concurrency.GenServers.Queue do
+  use GenServer
+
+  # An OTP server is a module with the GenServer behavior that implements a set of callbacks. At its most basic level a GenServer is a single process which runs a loop that handles one message per iteration passing along an updated state.
+
+  def start_link(state \\ []) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def init(state), do: {:ok, state}
+
+  # It’s often necessary to interact with GenServers in a synchronous way, calling a function and waiting for its response. To handle synchronous requests we need to implement the GenServer.handle_call/3 callback which takes: the request, the caller’s PID, and the existing state; it is expected to reply by returning a tuple: {:reply, response, state}.
+
+  # synchronous
+
+  def handle_call(:dequeue, _from, [first | rest]) do
+    {:reply, first, rest}
+  end
+
+  def handle_call(:dequeue, _from, []) do
+    {:reply, nil, []}
+  end
+
+  def handle_call(:queue, _from, state) do
+    {:reply, state, state}
+  end
+
+  def queue, do: GenServer.call(__MODULE__, :queue)
+  def dequeue, do: GenServer.call(__MODULE__, :dequeue)
+end
